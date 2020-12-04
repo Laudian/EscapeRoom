5483daf27d605f1fede2bbf9e4884e71c820d9b5
mrabe <mrabe@uni-bremen.de>
2020-12-04 14:53:54 +0100

    added entry for discord token in Settings.py

--- Settings.py
+++ Settings.py
@@ -4,3 +4,4 @@ import datetime
 DEBUG = True
 loglevel = logging.DEBUG
 logfile = "log/EscapeRoom_" + str(datetime.datetime.now())[:-7].replace("-", "_").replace(" ", "-").replace(":", "_") + ".log"
+discord_token = "12345"
