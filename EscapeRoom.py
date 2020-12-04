fba7c07480911579b1e44039c15461c5155aaff8
Josua Jeuck <josua@jeuck.de>
2020-12-04 15:33:02 +0100

    Klasse für discord bot erstellt, und bot start hinzugefügt

--- EscapeRoom.py
+++ EscapeRoom.py
@@ -1,8 +1,14 @@
 import logging
 import Settings_local as Settings
+import Discord
 
 # Set up logging
 logging.basicConfig(filename=Settings.logfile, level=Settings.loglevel,
                     format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 
 logging.info("A new EscapeRoom has begun!")
+
+# starting bot
+bot = Discord.DiscordBot
+bot.run(Settings.discord_token)
+
