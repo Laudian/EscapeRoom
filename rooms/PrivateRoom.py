b70350dd94439574ef04034b00f35fc68c5e37c8
mrabe <mrabe@uni-bremen.de>
2021-02-02 15:11:00 +0100

    Added PrivateRoom for real this time

---
+++ rooms/PrivateRoom.py
@@ -0,0 +1,30 @@
+from Room import Room
+import asyncio
+
+
+class PrivateRoom(Room):
+    def __init__(self, parent: Room, nameappend=""):
+        super().__init__(parent.name+nameappend, parent.game)
+        self.parent = parent
+        self.topic = parent.topic
+
+    async def setup(self):
+        await self.parent.game.setup_room(self, self.parent.game.categoryrooms, self.parent)
+
+    # This Method is called if the command used is unavailable in this room. It will inform the player
+    # of this by whispering to him
+    async def handle_invalid_command(self, caller, command, content):
+        await self.parent.handle_invalid_command(caller, command, content)
+        return
+
+    # This method handles commands that players use and should be called by the game commandHandler
+    # Usually,  every command should have it's own function which is accessed via a dicitonary
+    async def handle_command(self, caller, command, content=None):
+        await self.parent.handle_command(caller, command, content)
+        return
+
+    def __repr__(self):
+        return self.parent.name
+
+    def log(self, message: str):
+        self.parent.log(message)
