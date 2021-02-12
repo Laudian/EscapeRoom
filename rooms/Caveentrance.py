9a51f7478dac41a10b68114cb9c55f98c06d6e1f
Josua Jeuck <josua@jeuck.de>
2021-02-12 12:05:49 +0100

    caveentrance now working properly for 8 players

--- rooms/Caveentrance.py
+++ rooms/Caveentrance.py
@@ -6,31 +6,29 @@ class Caveentrance(Room):
     def __init__(self, game):
         super().__init__("Höhleneingang", game)
         # variables
-        self.torches = {0: False, 1: False, 2: False,
-                        3: False, 4: False, 5: False,
-                        6: False, 7: False, 8: False}
-        self.buttons = {0: [0, 1, 2, 3, 6], 1: [0, 1, 2, 4, 7], 2: [0, 1, 2, 5, 8],
-                        3: [3, 4, 5, 0, 6], 4: [3, 4, 5, 1, 7], 5: [3, 4, 5, 2, 8],
-                        6: [6, 7, 8, 0, 3], 7: [6, 7, 8, 1, 4], 8: [6, 7, 8, 2, 5]}
+        self.torches = {0: False, 1: False, 2: False, 3: False,
+                        4: False, 5: False, 6: False, 7: False}
+        self.buttons = {0: [0, 1, 2, 3, 4], 1: [0, 1, 2, 3, 5], 2: [0, 1, 2, 3, 6], 3: [0, 1, 2, 3, 7],
+                        4: [4, 5, 6, 7, 0], 5: [4, 5, 6, 7, 1], 6: [4, 5, 6, 7, 2], 7: [4, 5, 6, 7, 3]}
 
         # register commands
         self.register_command("button", self.pushButton, "Eigenen Knopf drücken - Höhleneingang")
 
-    async def printTorches(self, player):
+    async def printTorches(self):
         message = ""
-        for x in range(9):
+        for x in range(8):
             if self.torches[x]:
                 message += "O"
             else:
                 message += "X"
-            if x % 3 == 2:
+            if x % 4 == 3:
                 message += "\n"
         self.send(message)
 
     async def enter(self, player):
         await super().enter(player)
         if len(self.players) == 8:
-            await self.printTorches(player)
+            await self.printTorches()
         else:
             self.send("Warte auf weitere Spieler")
 
@@ -39,4 +37,15 @@ class Caveentrance(Room):
         changing_torches = self.buttons[button_number]
         for torch in changing_torches:
             self.torches[torch] = not self.torches[torch]
-        await self.printTorches(player)
+        if (True in self.torches.values()) != (False in self.torches.values()):
+            await self.printTorches()
+            await self.rewardPlayers()
+        else:
+            await self.printTorches()
+
+    async def rewardPlayers(self):
+        self.send("Nachricht am Ende des Raumes")  #TODO
+        nextroom = self.game.get_room("Eingangshalle")
+        for player in self.players:
+            await self.leave(player)
+            await nextroom.enter(player)
